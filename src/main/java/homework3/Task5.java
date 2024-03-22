7d5fd27893d45c6e87a6733903ac642ea2e47b66
Maxim Orlatiy <ihospik@yahoo.com>
2024-03-22 15:55:31 +0200

    Java homework 3

---
+++ src/main/java/homework3/Task5.java
@@ -0,0 +1,13 @@
+package homework3;
+
+public class Task5 {
+    public static void main(String[] args) {
+        double celsium = 5;
+        double fahrenheit = (celsium * 9 / 5) + 32;
+        double kelvin = celsium + 273.15;
+
+        System.out.println(" Celsium: " + celsium);
+        System.out.println(" Fahrenheit: " + fahrenheit);
+        System.out.println(" Kelvin: " + kelvin);
+    }
+}
